f40a8adee7df10d621f98eed13fa5124ea8c9ee0
liangliang <liangliang0918@126.com>
2014-11-05 10:16:13 +0800

    minit commit, two problems.

---
+++ MaximumProductSubarry/MaximumProductSubarry/main.cpp
@@ -0,0 +1,87 @@
+//
+//  main.cpp
+//  MaximumProductSubarry
+//
+//  Created by liang on 10/26/14.
+//  Copyright (c) 2014 Tina. All rights reserved.
+//
+
+#include <iostream>
+
+class Solution {
+public:
+    int maxProduct(int A[], int n) {
+        int max = 0;
+        int max1 = 1;
+        int max2 = 1;
+        int negative = 0;
+        if (n == 1) {
+            return A[0];
+        }
+        for (int i = 0; i < n; i++ ) {
+            if (A[i] == 0) {
+                int maxr = 0;
+                if (i > 0) {
+                    maxr = this->maxProduct(A, i);;
+                }
+                int maxl = 0;
+                if ((n - i - 1) > 0) {
+                    maxl =this->maxProduct(A + i + 1, n - i - 1);
+                }
+                if (maxr > maxl) {
+                    max = maxr;
+                } else {
+                    max = maxl;
+                }
+                if (max < 0) {
+                    max = 0;
+                }
+                return max;
+            }
+            max1 *= A[i];
+            if (negative > 0) {
+                max2 *= A[i];
+            }
+            if (A[i] < 0) {
+                negative++;
+            }
+        }
+        if (negative%2 == 0 && max1 > max) {
+            max = max1;
+        } else {
+            int max3 = 1;
+            negative = 0;
+            for (int i = n -1; i >= 0; i-- ) {
+                if (negative > 0) {
+                    max3 *= A[i];
+                }
+                if (A[i] < 0) {
+                    negative++;
+                }
+            }
+            if (max2 > max3 && max2 > max) {
+                max = max2;
+            } else if (max3 > max) {
+                max = max3;
+            }
+        }
+        return max;
+    }
+};
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    Solution *s = new Solution();
+    int a[] = {3, 2, 0, 4, 5, 9, 0, 10, 10, 10};
+//    int a[] = {-2, 0, -1};
+//    int a[] = {0, -2, 0};
+    std::cout << s->maxProduct(a, 10);
+    return 0;
+}
+
+
+
+
+
---
+++ Reverse Words in a String/Reverse Words in a String/main.cpp
@@ -0,0 +1,63 @@
+//
+//  main.cpp
+//  Reverse Words in a String
+//
+//  Created by liang on 11/3/14.
+//  Copyright (c) 2014 Tina. All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+template <class T> class ReverseWords : public unary_function<T, void> {
+    public :
+    ReverseWords(string &o, string &word) : _o(o), _word(word) {}
+    void operator() (T x) {
+        if (x == ' ' && !_word.empty()) {
+            _o.append(_word.rbegin(), _word.rend());
+            _o.push_back(' ');
+            _word.clear();
+        } else if (x != ' ') {
+            _word.push_back(x);
+        }
+    }
+    void outEndWord() {
+        if (!_word.empty()) {
+            _o.append(_word.rbegin(), _word.rend());
+        } else if (_o.size() > 1 && _o.back() == ' ') {
+            _o.erase(--_o.end());
+        }
+    }
+    private :
+    string &_o;
+    string &_word;
+};
+
+class Solution {
+public:
+    void reverseWords(string &s) {
+        string o;
+        string word;
+        ReverseWords<string::value_type> r = ReverseWords<string::value_type>(o, word);
+        for_each(s.rbegin(), s.rend(), r);
+        r.outEndWord();
+        s.swap(o);
+    }
+};
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+//    string str = "   the sky is   blue   ";
+//    string str = " 1";
+    string str = "";
+    cout << str << endl;
+    Solution *s = new Solution();
+    s->reverseWords(str);
+    cout << str << endl;
+    return 0;
+}
+
