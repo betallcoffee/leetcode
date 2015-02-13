dffe9daa91c20088950f6acd339bd77be5b22359
liangliang <liangliang0918@126.com>
2015-02-13 16:53:01 +0800

    maximun product subarry use formula.

--- MaximumProductSubarry/MaximumProductSubarry/main.cpp
+++ MaximumProductSubarry/MaximumProductSubarry/main.cpp
@@ -9,10 +9,47 @@
 //
 //For example, given the array [2,3,-2,4],
 //the contiguous subarray [2,3] has the largest product = 6.
+//Let us denote that:
+//
+//f(k) = Largest product subarray, from index 0 up to k.
+//
+//Similarly,
+//
+//g(k) = Smallest product subarray, from index 0 up to k.
+//
+//Then,
+//
+//f(k) = max( f(k-1) * A[k], A[k], g(k-1) * A[k] )
+//g(k) = min( g(k-1) * A[k], A[k], f(k-1) * A[k] )
+
 
 #include <iostream>
+#include <vector>
 
 class Solution {
+public:
+    int maxProduct(int A[], int n) {
+        int res = A[0];
+        std::vector<int> max(n);
+        std::vector<int> min(n);
+        max[0] = A[0];
+        min[0] = A[0];
+        for (int i = 1; i < n ; ++i) {
+            int a, b, c;
+            a = max[i-1] * A[i];
+            b = A[i];
+            c = min[i-1] * A[i];
+            max[i] = std::max(std::max(a, b), c);
+            min[i] = std::min(std::min(a, b), c);
+            if (res < max[i]) {
+                res = max[i];
+            }
+        }
+        return res;
+    }
+};
+
+class Solution1 {
 public:
     int maxProduct(int A[], int n) {
         int max = 0;
@@ -78,10 +115,14 @@ int main(int argc, const char * argv[])
 
     // insert code here...
     Solution *s = new Solution();
-    int a[] = {3, 2, 0, 4, 5, 9, 0, 10, 10, 10};
-//    int a[] = {-2, 0, -1};
-//    int a[] = {0, -2, 0};
-    std::cout << s->maxProduct(a, 10);
+    int a1[] = {3, 2, 0, 4, 5, 9, 0, 10, 10, 10};
+    std::cout << s->maxProduct(a1, 10) << std::endl;
+    int a2[] = {-2, 0, -1};
+    std::cout << s->maxProduct(a2, 3) << std::endl;
+    int a3[] = {0, -2, 0};
+    std::cout << s->maxProduct(a3, 3) << std::endl;
+    int a4[] = {-2,3,-4};
+    std::cout << s->maxProduct(a4, 3) << std::endl;
     return 0;
 }
 
