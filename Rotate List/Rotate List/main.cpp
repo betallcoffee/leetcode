b6ebabc5d5721e784b8c5b666bec3dc29d7cf5bb
liangliang <liangliang0918@126.com>
2018-09-16 15:17:06 +0800

    Rotate List

---
+++ Rotate List/Rotate List/main.cpp
@@ -0,0 +1,82 @@
+//
+//  main.cpp
+//  Rotate List
+//
+//  Created by liang on 15/9/18.
+//  Copyright © 2018年 liang. All rights reserved.
+//  https://leetcode.com/problems/rotate-list/description/
+/**
+ Given a linked list, rotate the list to the right by k places, where k is non-negative.
+ 
+ Example 1:
+ 
+ Input: 1->2->3->4->5->NULL, k = 2
+ Output: 4->5->1->2->3->NULL
+ Explanation:
+ rotate 1 steps to the right: 5->1->2->3->4->NULL
+ rotate 2 steps to the right: 4->5->1->2->3->NULL
+ Example 2:
+ 
+ Input: 0->1->2->NULL, k = 4
+ Output: 2->0->1->NULL
+ Explanation:
+ rotate 1 steps to the right: 2->0->1->NULL
+ rotate 2 steps to the right: 1->2->0->NULL
+ rotate 3 steps to the right: 0->1->2->NULL
+ rotate 4 steps to the right: 2->0->1->NULL
+ */
+
+#include <iostream>
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        if (k == 0) {
+            return head;
+        }
+        int length = 0;
+        ListNode *cur = head;
+        while (cur) {
+            length++;
+            cur = cur->next;
+        }
+        k = length - k % length;
+        cur = head;
+        ListNode *pre = nullptr;
+        ListNode *result = nullptr;
+        while (cur) {
+            pre = cur;
+            cur = cur->next;
+            k--;
+            if (k == 0 && cur != nullptr) {
+                pre->next = nullptr;
+                result = cur;
+            }
+        }
+        
+        if (result) {
+            pre->next = head;
+            return result;
+        } else {
+            return head;
+        } 
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
